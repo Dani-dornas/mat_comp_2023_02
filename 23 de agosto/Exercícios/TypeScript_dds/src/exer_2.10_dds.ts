e784fd9f02ee2115072a51d01192befb06f22022
Dani-dornas <lauby.dd@gmail.com>
2023-08-30 20:08:54 -0300

    finalizacao dos exercicios pt2

--- 23 de agosto/Exercícios/TypeScript_dds/src/exer_2.10_dds.ts
+++ 23 de agosto/Exercícios/TypeScript_dds/src/exer_2.10_dds.ts
@@ -1,13 +1,13 @@
-function fibonacciRecursivo(n: number): number {
+function fibonacciRecursivo_dds(n: number): number {
     if (n <= 0) {
         return 0;
     } else if (n === 1) {
         return 1;
     } else {
-        return fibonacciRecursivo(n - 1) + fibonacciRecursivo(n - 2);
+        return fibonacciRecursivo_dds(n - 1) + fibonacciRecursivo_dds(n - 2);
     }
 }
 
 const indi = 6;
-const result = fibonacciRecursivo(indi);
+const result = fibonacciRecursivo_dds(indi);
 console.log(`O termo na posição ${indi} da sequência de Fibonacci é: ${result}`);
--- 23 de agosto/Exercícios/TypeScript_dds/src/exer_2.9_dds.ts
+++ 23 de agosto/Exercícios/TypeScript_dds/src/exer_2.9_dds.ts
@@ -1,7 +1,7 @@
-function PG(termoInicial: number, razao: number, n: number): number {
+function PG_dds(termoInicial: number, razao: number, n: number): number {
     return termoInicial * Math.pow(razao, n - 1);
 }
 
 const n = 50;
-const exemplo = PG(1, 2, n)
+const exemplo = PG_dds(1, 2, n)
 console.log(`O termo na posição ${n} da progressão é: ${exemplo}`);
